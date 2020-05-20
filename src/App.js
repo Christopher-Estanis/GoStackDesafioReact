4fca32f250d7f3af03de79823e861062e9e43a6f
christopher-estanis <christopher.estanis@gmail.com>
2020-05-20 01:43:05 -0300

    Finish react challenge

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import api from './services/api'
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
+  async function handleAddRepository() {
+    // TODO
+
+    const response = await api.post(`/repositories`,{
+      url: 'url.com', 
+      title: 'titulo', 
+      techs: ['Techa', 'Techb']
+    })
+    
+    setRepositories([...repositories, response.data])
+  }
+
+  async function handleRemoveRepository(id) {
+
+    const newRepositories = repositories.filter(repository => repository.id !== id)
+
+    api.delete(`/repositories/${id}`).then(
+      setRepositories(newRepositories)
+    )
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+      </ul>
+      
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
